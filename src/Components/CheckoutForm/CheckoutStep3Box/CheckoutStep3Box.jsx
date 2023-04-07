4c9dd4c9ee26aa868b7cfde5484515ef41fd38bf
antu <antusaha970@gmail.com>
2023-04-07 13:41:00 +0600

    before adding confetti

--- src/Components/CheckoutForm/CheckoutStep3Box/CheckoutStep3Box.jsx
+++ src/Components/CheckoutForm/CheckoutStep3Box/CheckoutStep3Box.jsx
@@ -1,7 +1,44 @@
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const CheckoutStep3Box = () => {
-  return <div>box</div>;
+  const navigation = useNavigate();
+  const handleBackHome = () => {
+    navigation("/");
+  };
+
+  return (
+    <section>
+      <Container maxWidth="lg">
+        <Typography
+          variant="h5"
+          component="h3"
+          sx={{
+            fontWeight: "bold",
+            textAlign: "center",
+            margin: "20px 0",
+          }}
+        >
+          Congratulation Your Membership has been created
+          <br />
+          Please Check Your Email address for more information and your class
+          schedule
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "10px",
+          }}
+        >
+          <button className="btn-brand" onClick={handleBackHome}>
+            Back to home
+          </button>
+        </Box>
+      </Container>
+    </section>
+  );
 };
 
 export default CheckoutStep3Box;
