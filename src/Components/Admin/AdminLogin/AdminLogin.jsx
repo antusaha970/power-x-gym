7eea5676525a396d1d60d387f21161904a152c5b
antu <antusaha970@gmail.com>
2023-04-08 19:50:24 +0600

    added admin dashboard login component logic

--- src/Components/Admin/AdminLogin/AdminLogin.jsx
+++ src/Components/Admin/AdminLogin/AdminLogin.jsx
@@ -1,14 +1,42 @@
-import { Box, Container, TextField, Typography } from "@mui/material";
+import { AccountCircle, Email, Lock } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Container,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
+import { client } from "../../../Api/Client";
+import { useNavigate } from "react-router-dom";
 
 const AdminLogin = () => {
-  const { control, handleSubmit } = useForm({
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     defaultValues: {
       email: "",
+      password: "",
     },
   });
-  const onSubmit = (data) => console.log(data);
+  const navigation = useNavigate();
+  const onSubmit = async (data) => {
+    console.log(data);
+    try {
+      const response = await client.post("/isAdmin", data);
+      if (response.data) {
+        navigation("/admin/dashboard/menu");
+      } else {
+        alert("Wrong email or password");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <section>
       <Container maxWidth="lg">
@@ -30,16 +58,68 @@ const AdminLogin = () => {
             <Controller
               name="email"
               control={control}
+              rules={{ required: true }}
               render={({ field }) => (
                 <TextField
                   {...field}
                   id="outlined-basic"
-                  label="Outlined"
+                  label="Enter admin Email"
                   variant="outlined"
+                  fullWidth
+                  margin="normal"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Email />
+                      </InputAdornment>
+                    ),
+                  }}
                 />
               )}
             />
-            <button type="submit">submit</button>
+            {errors.email && (
+              <Typography variant="small" component="small" color="error">
+                This field is required
+              </Typography>
+            )}
+            <Controller
+              name="password"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  id="outlined-basic-pass"
+                  label="Enter admin password"
+                  variant="outlined"
+                  fullWidth
+                  margin="normal"
+                  type="password"
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Lock />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              )}
+            />
+            {errors.password && (
+              <Typography
+                variant="small"
+                component="small"
+                color="error"
+                sx={{
+                  display: "block",
+                }}
+              >
+                This field is required
+              </Typography>
+            )}
+            <Button variant="contained" type="submit">
+              Submit
+            </Button>
           </form>
         </Box>
       </Container>
